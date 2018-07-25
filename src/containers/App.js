3becbf69ea90c6a3ffa0090bcad7f56b7698a9ad
Paul Mcloughlin <cloggy45@googlemail.com>
2018-07-25 03:13:59 +0100

    Added simple ListThread

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 
 import NavBar from '../components/navbar';
-import ThreadCard from './ThreadCard';
-
+import ListThreads from '../containers/ListThreads';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar />
-        <ThreadCard />
-
+        <ListThreads />
       </div>
     );
   }
