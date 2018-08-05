c02061a2762ef2543d5ef60da8b9c49c88ee25d9
Paul Mcloughlin <cloggy45@googlemail.com>
2018-08-05 09:25:28 +0100

    Updated gitignore to include the example

---
+++ src/misc/connectionInfo-EXAMPLE.js
@@ -0,0 +1,12 @@
+export const serverInfo = {
+    address: "API-URL"
+};
+
+export const authInfo = {
+    AUTH0_DOMAIN: 'DOMAIN',
+    AUTH0_CLIENT_ID: 'CLIENT',
+    REDIRECT_URL: 'REDIRECT',
+    audience: 'AUDIENCE',
+    responseType: 'token id_token',
+    scope: 'openid'
+};
